b0ad4c6ed5293cb03b2d0c018670094484624f10
JeanCarloH <55773226+JeanCarloH@users.noreply.github.com>
2022-05-03 19:02:38 -0500

    comentario terminado

--- preguntas.py
+++ preguntas.py
@@ -145,5 +145,5 @@ def pregunta_04():
 
     # Compute and print R^2 and RMSE
     print("R^2: {:6.4f}".format(linearRegression.score(X_test, y_test)))
-    rmse = np.sqrt(mean_squared_error(y_test, y_life))
+    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
     print("Root Mean Squared Error: {:6.4f}".format(rmse))
